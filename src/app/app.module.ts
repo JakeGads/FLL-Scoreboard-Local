63f2b76ab3d8273c23723be89347d766daf276a1
Jake Gadaleta <jgadaleta@ceiamerica.com>
2021-09-13 14:39:33 -0400

    added route definations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,16 @@ import { SettingsComponent } from './settings/settings.component';
 import { DataEntryComponent } from './data-entry/data-entry.component';
 import { BoardComponent } from './board/board.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { SettingPageComponent } from './pages/setting-page/setting-page.component';
+
+export const routes: Routes = [
+  { path: 'board', component: BoardPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'settings', component: SettingPageComponent},
+];
 
 @NgModule({
   declarations: [
@@ -17,9 +27,13 @@ import { NavbarComponent } from './navbar/navbar.component';
     SettingsComponent,
     DataEntryComponent,
     BoardComponent,
-    NavbarComponent
+    NavbarComponent,
+    BoardPageComponent,
+    AdminPageComponent,
+    SettingPageComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     FormsModule,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { routes } from '../app.module'
 
 @Component({
   selector: 'app-navbar',
