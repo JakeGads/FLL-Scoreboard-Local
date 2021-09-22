79e898b1e4e474a738c4179a0f10c314e323ecba
Jake Gadaleta <jgadaleta@ceiamerica.com>
2021-09-22 11:45:25 -0400

    added add team functionality

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
 // use request to avoid a ts-node-dev error
 const express = require( "express" );
 const fs = require('fs');
+
+const multer = require('multer');
+const csv = require('fast-csv');
+
 const app = express();
 
 const teamFile = 'server/Teams.json'
-
-
 app.use(
     (req: any, res: any, next: any) => {
         res.header('Access-Control-Allow-Origin', '*');
@@ -21,32 +23,67 @@ app.use(
     }
 )
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/getTeams', (req: any, res: any) => {
     fs.readFile(teamFile, 'utf8', (err:any, data:any) => {
         if (err) {
-            console.error(err)
-            res.send(err)
-            return
+            console.error(err);
+            res.send(err);
+            return;
           }
-        res.send(data)
+        else{
+            res.send(data);
+            return;
+        }
     })
-})
+});
 
 app.put('/saveTeams', (req: any, res: any) => {
     fs.writeFile(teamFile, req.query['teams'], (err: any) => {
         if(err){
-            console.log(err)
-            res.sendStatus(400)
-        }
-        else{
-            console.log(req.query['teams'])
-            console.log('Write Successful')
+            console.log(err);
+            res.sendStatus(400);
+            return;
         }
     })
     res.sendStatus(200)
-})
+});
+
+const upload = multer({ dest: 'tmp/csv/' });
+app.use('/add-teams', upload.single('file'), function (req:any, res:any) {
+    const fileRows: string[] = [];
+    // open uploaded file
+    csv.fromPath(req.file.path)
+      .on("data", function (data: any) {
+        fileRows.push(data); 
+      })
+      .on("end", function () {
+        console.log(fileRows)
+        fs.unlinkSync(req.file.path);   
+        //process "fileRows" and respond
+      })
+});
+
+app.get('/add-team'), (req: any, res: any) => {
+    fs.readFile(teamFile, 'utf8', (err:any, data:any) => {
+        if (err) {
+            console.error(err);
+            res.send(err);
+            return;
+          }
+        else{
+            fs.appendFile(teamFile, data.replace('}]', `}${req.query['team']}`, (err: any) => {
+                if (err) {
+                  console.log(err);
+                }
+            }));
+            res.send(data);
+            return;
+        }
+    })
+};
+
 
 app.listen(4201, '127.0.0.1', function() {
     console.log('API serving on 4201')
