1b28690270490f32e7a9233f06c3b4192163ec64
Jake Gadaleta <jake.gads@gmail.com>
2021-09-28 14:03:18 -0400

    syncing for shift to another pc

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -11,31 +11,30 @@ export class TeamService{
   
   teams: Team[] = [];
   source: BehaviorSubject<Team[]> = new BehaviorSubject(this.teams);
-  current:Observable<Team[]> = this.source.asObservable();;
+  current:Observable<Team[]> = this.source.asObservable();
 
   constructor(private settingService: SettingsService, private http: HttpClient){    // TODO pull data from the API when configure
     this.current = this.http.get<Team[]>('http://localhost:4201/getTeams')
     this.current.subscribe(message => this.teams = message)
+    
     for(let i = 0; i < this.teams.length; i++){
         this.teams[i] = new Team(this.teams[i].name, this.teams[i].num, this.teams[i].scores);
         console.log(this.teams[i])
     }
+
     console.log(`TEAMS: ${this.teams}`)
   }
   
-  private sortTeams() {
-      
-      this.teams = this.teams.sort(
-          function(a: Team, b: Team){
-              if(a.avg == b.avg){
-                  if(a.orderedScores[0] == b.orderedScores[0]){
-                      return b.num - a.num
-                  }
-                  return b.orderedScores[0] - a.orderedScores[0]
-              }   
-              return b.avg - a.avg;  
-          }
-      )
+  private sortTeams() { 
+    this.teams = this.teams.sort( (a: Team, b: Team) => {
+        if(a.avg == b.avg){
+            if(a.orderedScores[0] == b.orderedScores[0]){
+                return b.num - a.num
+            }
+            return b.orderedScores[0] - a.orderedScores[0]
+        }   
+        return b.avg - a.avg;  
+    });
   }
 
   public addScore(teamNumber: number, score: number, top: number = 0){
